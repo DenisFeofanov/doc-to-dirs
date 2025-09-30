2851c300779835647802c1adc62b9b5a4dd35c39
Feofanov Denis <omail4me@gmail.com>
2025-09-30 16:25:57 +0500

    chore: remove multer since we don't use it yet

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,3 +1,4 @@
+// all env variables in one place without business logic
 export default {
   port: process.env.PORT || 3000,
   nodeEnv: process.env.NODE_ENV || "production", // production || development
