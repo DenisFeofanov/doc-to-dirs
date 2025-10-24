1ed86d8b95140ccc6e227d8dad2609948c3f00b7
Feofanov Denis <omail4me@gmail.com>
2025-10-24 13:01:52 +0500

    chore: move eslint from backend to project directory

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig([
     files: ["**/*.{js,mjs,cjs}"],
     plugins: { js },
     extends: ["js/recommended"],
-    languageOptions: { globals: globals.node },
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
     rules: {
       "no-unused-vars": ["warn", { args: "all", argsIgnorePattern: "^_" }],
     },
